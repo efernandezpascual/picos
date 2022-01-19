ed29b7f82be04b06f34e5ffecc01c0a421d9b58d
efernandezpascual <61799380+efernandezpascual@users.noreply.github.com>
2022-01-19 13:50:13 +0100

    4 x PCA

--- src/figure-pca-temperatures.R
+++ src/figure-pca-temperatures.R
@@ -1,9 +1,45 @@
 library(tidyverse)
 
+### Temporal survey
+
+read.csv("data/temporal-survey-temperatures.csv") %>%
+  mutate(Time = as.POSIXct(Time, tz = "UTC")) %>% 
+  mutate(Month = lubridate::month(Time)) %>% 
+  mutate(Day = lubridate::day(Time)) %>% 
+  mutate(Hour = lubridate::hour(Time)) %>% 
+  filter(Hour %in% c(0, 4, 8, 12, 16, 20)) %>% # Keep same recording hours as ibuttons
+  filter(! Month %in% 9) %>% # Remove missing September days
+  filter(! (Month %in% 8 & Day > 6)) %>% # Remove missing August days
+  filter(! (Month %in% 10 & Day < 3)) %>% # Remove missing October days %>%
+  dplyr::select(-c(Month, Day, Hour)) %>%
+  group_by(Site, Day = lubridate::floor_date(Time, "day")) %>%
+  summarise(T = mean(Temperature), X = max(Temperature), N = min(Temperature), n = length(Time)) %>% # Daily mean, max, min
+  mutate(Snow = ifelse(X < 0.5 & N > -0.5, 1, 0)) %>% # Day is snow day or not
+  # mutate(Snow = ifelse((X - N) <= 0.5, 1, 0)) %>% # Day is snow day or not
+  mutate(FreezeThaw = ifelse(X > 0.5 & N < -0.5, 1, 0)) %>% # Day with freeze-thaw cycles
+  mutate(FDD = ifelse(T < 0, T, 0)) %>% # Freezing degrees per day
+  mutate(GDD = ifelse(T >= 1, T, 0)) %>% # Growing degrees day per month https://link.springer.com/article/10.1007/s00035-021-00250-1
+  group_by(Site, Month = lubridate::floor_date(Day, "month")) %>%
+  summarise(T = mean(T), X = mean(X), N = mean(N), # Daily mean, max, min
+            Snow = sum(Snow), # Snow days per month
+            FreezeThaw = sum(FreezeThaw), # Freeze-thaw days per month
+            FDD = sum(FDD), # FDD per month
+            GDD = sum(GDD)) %>% # GDD per month
+  group_by(Site, Year = lubridate::floor_date(Month, "year")) %>%
+  summarise(bio1 = mean(T), # Annual Mean Temperature
+            bio2 = mean(X - N), # Mean Diurnal Range (Mean of monthly (max temp - min temp))
+            bio7 = max(X) - min(N), # Temperature Annual Range (BIO5-BIO6)
+            FDD = abs(sum(FDD)), # FDD per year
+            GDD = sum(GDD)) %>%
+  mutate(Year = as.character(lubridate::year(Year))) %>% 
+  rename(Plot = Year) %>%
+  mutate(Survey = "Temporal") -> 
+  dfTime
+
 ### Spatial survey data
 
 read.csv("data/spatial-survey-temperatures.csv") %>%
-  mutate(Time = as.POSIXct(Time, tz = "UTC")) %>% 
+  mutate(Time = as.POSIXct(Time, tz = "UTC")) %>%
   merge(read.csv("data/spatial-survey-header.csv")) %>%
   group_by(Plot, Site, Day = lubridate::floor_date(Time, "day")) %>%
   summarise(T = mean(Temperature), X = max(Temperature), N = min(Temperature), n = length(Time)) %>% # Daily mean, max, min
@@ -11,34 +47,90 @@ read.csv("data/spatial-survey-temperatures.csv") %>%
   mutate(FreezeThaw = ifelse(X > 0.5 & N < -0.5, 1, 0)) %>% # Day with freeze-thaw cycles
   mutate(FDD = ifelse(T < 0, T, 0)) %>% # Freezing degrees per day
   mutate(GDD = ifelse(T >= 1, T, 0)) %>% # Growing degrees day per month https://link.springer.com/article/10.1007/s00035-021-00250-1
-  group_by(Plot, Site, Month = lubridate::floor_date(Day, "month")) %>%
+  group_by(Site, Plot, Month = lubridate::floor_date(Day, "month")) %>%
   summarise(T = mean(T), X = mean(X), N = mean(N), # Daily mean, max, min
             Snow = sum(Snow), # Snow days per month
             FreezeThaw = sum(FreezeThaw), # Freeze-thaw days per month
             FDD = sum(FDD), # FDD per month
             GDD = sum(GDD)) %>% # GDD per month
-  group_by(Plot, Site) %>%
-  summarise(`Annual\ntemperature` = mean(T), # Annual Mean Temperature
-            `Diurnal range` = mean(X - N), # Mean Diurnal Range (Mean of monthly (max temp - min temp))
-            `Annual range` = max(X) - min(N), # Temperature Annual Range (BIO5-BIO6)
-            `Freezing\ndegrees-day` = abs(sum(FDD)), # FDD per year
-            `Growing\ndegrees-day` = sum(GDD)) -> # GDD per year
-  micro
+  group_by(Site, Plot) %>%
+  summarise(bio1 = mean(T), # Annual Mean Temperature
+            bio2 = mean(X - N), # Mean Diurnal Range (Mean of monthly (max temp - min temp))
+            bio7 = max(X) - min(N), # Temperature Annual Range (BIO5-BIO6)
+            FDD = abs(sum(FDD)), # FDD per year
+            GDD = sum(GDD)) %>%
+  mutate(Survey = "Spatial") -> # GDD per year
+  dfSpace
 
-### PCA
+### Merge
 
-micro[, 3:7] %>%
-  FactoMineR::PCA() -> pca
+rbind(dfSpace, dfTime) -> bioclim
+bioclim %>% filter(Site == "Los Cazadores") -> bioclim1
+bioclim %>% filter(Site == "Hou Sin Tierri") -> bioclim2
+bioclim %>% filter(Site == "Los Boches") -> bioclim3
+bioclim %>% filter(Site == "Hoyo Sin Tierra") -> bioclim4
 
-cbind((micro %>%  dplyr::select(Plot, Site)), data.frame(pca$ind$coord[, 1:2])) %>%
+### PCA 1
+
+bioclim1[, 3:7] %>%
+  FactoMineR::PCA() -> pca1
+
+cbind((bioclim1 %>%  dplyr::select(Plot, Site, Survey)), data.frame(pca1$ind$coord[, 1:2])) -> pcaInds1
+
+pca1$var$coord[, 1:2] %>%
+  data.frame %>%
+  rownames_to_column(var = "Variable") %>%
+  mutate(Site = "Los Cazadores") -> pcaVars1
+
+### PCA 2
+
+bioclim2[, 3:7] %>%
+  FactoMineR::PCA() -> pca2
+
+cbind((bioclim2 %>%  dplyr::select(Plot, Site, Survey)), data.frame(pca2$ind$coord[, 1:2])) -> pcaInds2
+
+pca2$var$coord[, 1:2] %>%
+  data.frame %>%
+  rownames_to_column(var = "Variable") %>%
+  mutate(Site = "Hou Sin Tierri") -> pcaVars2
+
+### PCA 3
+
+bioclim3[, 3:7] %>%
+  FactoMineR::PCA() -> pca3
+
+cbind((bioclim3 %>%  dplyr::select(Plot, Site, Survey)), data.frame(pca3$ind$coord[, 1:2])) -> pcaInds3
+
+pca3$var$coord[, 1:2] %>%
+  data.frame %>%
+  rownames_to_column(var = "Variable") %>%
+  mutate(Site = "Los Boches") -> pcaVars3
+
+### PCA 4
+
+bioclim4[, 3:7] %>%
+  FactoMineR::PCA() -> pca4
+
+cbind((bioclim4 %>%  dplyr::select(Plot, Site, Survey)), data.frame(pca4$ind$coord[, 1:2])) -> pcaInds4
+
+pca4$var$coord[, 1:2] %>%
+  data.frame %>%
+  rownames_to_column(var = "Variable") %>%
+  mutate(Site = "Hoyo Sin Tierra") -> pcaVars4
+
+### Merge PCA results
+
+rbind(pcaInds1, pcaInds2, pcaInds3, pcaInds4) %>%
   mutate(Site = fct_relevel(Site,
                             "Los Cazadores", "Hou Sin Tierri",
                             "Los Boches", "Hoyo Sin Tierra")) %>%
   mutate(Site = fct_recode(Site, "Ḥou Sin Tierri" = "Hou Sin Tierri")) -> pcaInds
 
-pca$var$coord[, 1:2] %>%
-  data.frame %>%
-  rownames_to_column(var = "Variable") -> pcaVars
+rbind(pcaVars1, pcaVars2, pcaVars3, pcaVars4) %>%
+  mutate(Site = fct_relevel(Site,
+                            "Los Cazadores", "Hou Sin Tierri",
+                            "Los Boches", "Hoyo Sin Tierra")) %>%
+  mutate(Site = fct_recode(Site, "Ḥou Sin Tierri" = "Hou Sin Tierri")) -> pcaVars
 
 ### Plot PCA
 
@@ -48,7 +140,8 @@ ggplot(pcaInds, aes(x = Dim.1, y = Dim.2)) +
   geom_vline(xintercept = 0, linetype = "dashed") +
   geom_segment(data = pcaVars, aes(x = 0, y = 0, xend = 3*Dim.1, yend = 3*Dim.2)) +
   ggthemes::theme_tufte() + 
-  geom_point(aes(fill = Site), size = 6, shape = 22) +
+  facet_wrap(~ Site) + 
+  geom_text(aes(label = Plot, color = Survey), size = 4) +
   theme(legend.position = "top", 
         legend.title = element_blank(),
         # legend.text = element_text(size = 12, color = "black"),
@@ -56,10 +149,8 @@ ggplot(pcaInds, aes(x = Dim.1, y = Dim.2)) +
         axis.title = element_text(size = 12),
         axis.text = element_text(size = 12, color = "black"),
         plot.margin = unit(c(0, 0.1, 0, 0), "cm")) +
-  scale_x_continuous(name = paste("Axis 1 (", round(pca$eig[1, 2], 0),
-                                  "% variance explained)", sep = "")) + 
-  scale_y_continuous(name = paste("Axis 2 (", round(pca$eig[2, 2], 0), 
-                                  "% variance explained)", sep = "")) +
+  scale_x_continuous(name = "PC1") + 
+  scale_y_continuous(name = "PC2") +
   geom_label(data = pcaVars, aes(x = 3*Dim.1, y = 3*Dim.2, label = Variable),  show.legend = FALSE, size = 4) +
   scale_color_manual(values = c("goldenrod", "forestgreen",  "royalblue", "darkorchid")) +
   scale_fill_manual(values = c("goldenrod", "forestgreen",  "royalblue", "darkorchid")) -> f1; f1
@@ -67,6 +158,6 @@ ggplot(pcaInds, aes(x = Dim.1, y = Dim.2)) +
 ### Save figure
 
 ggsave(f1, file = "results/figures/pca-temperatures.png", 
-       path = NULL, scale = 1, width = 127, height = 118, units = "mm", dpi = 600)
+       path = NULL, scale = 1, width = 182, height = 182, units = "mm", dpi = 600)
 # ggsave(f1, file = "results/figures/pca-temperatures.tiff", device = grDevices::tiff, 
 #        path = NULL, scale = 1, width = 182, height = 182, units = "mm", dpi = 600, compression = "lzw")
--- src/figure-temperature-logs.R
+++ src/figure-temperature-logs.R
@@ -1,33 +1,6 @@
 library(tidyverse); library(urca); library(raster); library(tibbletime); library(lubridate)
 Sys.setlocale("LC_TIME", "English")
 
-### Geoprecision logs
-
-read.csv("data/temporal-survey-temperatures.csv") %>% 
-  mutate(Time = as.POSIXct(Time, tz = "UTC")) %>%
-  mutate(Site = fct_relevel(Site,
-                            "Los Cazadores", "Hou Sin Tierri",
-                            "Los Boches", "Hoyo Sin Tierra")) %>%
-  mutate(Site = fct_recode(Site, "Ḥou Sin Tierri" = "Hou Sin Tierri")) %>%
-  ggplot(aes(Time, Temperature)) + 
-  facet_wrap(~ Site, nrow = 1) + 
-  geom_line(color = "red") +
-  labs(title = "(C) Hourly logs, temporal survey (2009-2018)") +
-  xlab("Time (1-h recording inverval)") +
-  ylab("Temperature (ºC)") +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  ggthemes::theme_tufte() +
-  theme(strip.background = element_rect(colour = "grey96", fill = "grey96"),
-        legend.position = "none", 
-        legend.direction = "vertical",
-        legend.title = element_blank(),
-        legend.text = element_text(size = 16, face = "italic"), 
-        panel.background = element_rect(color = "grey96", fill = "grey96"),
-        strip.text = element_text(size = 12),
-        axis.title = element_text(size = 12),
-        axis.text.x = element_text(size = 8, color = "black"),
-        axis.text.y = element_text(size = 11, color = "black")) -> f1; f1
-
 ### iButton logs
 
 read.csv("data/spatial-survey-temperatures.csv") %>%
@@ -41,10 +14,39 @@ read.csv("data/spatial-survey-temperatures.csv") %>%
   facet_wrap(~ Site, nrow = 1) + 
   geom_hline(yintercept = 0, linetype = "dashed") +
   geom_line() +
-  labs(title = "(D) Hourly logs, spatial survey (20 iButtons per site, Oct 2018 - Aug 2019)") +
+  labs(title = "(A) Hourly logs, spatial survey (20 iButtons per site, Oct 2018 - Aug 2019)") +
   xlab("Time (4-h recording inverval)") +
   ylab("Temperature (ºC)") +
   ggthemes::theme_tufte() +
+  coord_cartesian(ylim = c(-10, 30)) +
+  theme(strip.background = element_rect(colour = "grey96", fill = "grey96"),
+        legend.position = "none", 
+        legend.direction = "vertical",
+        legend.title = element_blank(),
+        legend.text = element_text(size = 16, face = "italic"), 
+        panel.background = element_rect(color = "grey96", fill = "grey96"),
+        strip.text = element_text(size = 12),
+        axis.title = element_text(size = 12),
+        axis.text.x = element_text(size = 8, color = "black"),
+        axis.text.y = element_text(size = 11, color = "black")) -> f1; f1
+
+### Geoprecision logs
+
+read.csv("data/temporal-survey-temperatures.csv") %>% 
+  mutate(Time = as.POSIXct(Time, tz = "UTC")) %>%
+  mutate(Site = fct_relevel(Site,
+                            "Los Cazadores", "Hou Sin Tierri",
+                            "Los Boches", "Hoyo Sin Tierra")) %>%
+  mutate(Site = fct_recode(Site, "Ḥou Sin Tierri" = "Hou Sin Tierri")) %>%
+  ggplot(aes(Time, Temperature)) + 
+  facet_wrap(~ Site, nrow = 1) + 
+  geom_line(color = "red") +
+  labs(title = "(B) Hourly logs, temporal survey (2009-2018)") +
+  xlab("Time (1-h recording inverval)") +
+  ylab("Temperature (ºC)") +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  ggthemes::theme_tufte() +
+  coord_cartesian(ylim = c(-10, 30)) +
   theme(strip.background = element_rect(colour = "grey96", fill = "grey96"),
         legend.position = "none", 
         legend.direction = "vertical",
@@ -90,7 +92,7 @@ trends %>%
   facet_wrap(~ Site, nrow = 1) + 
   geom_smooth(method = "lm", color = "black", size = .5, linetype = "dashed") +
   geom_line(color = "darkorange") +
-  labs(title = "(B) 10-year trend decomposition, temporal survey (2009-2018)") +
+  labs(title = "(C) 10-year trend decomposition, temporal survey (2009-2018)") +
   xlab("Time (1-h recording inverval)") +
   ylab("Temperature (ºC)") +
   ggthemes::theme_tufte()  +
@@ -105,57 +107,14 @@ trends %>%
         axis.text.x = element_text(size = 8, color = "black"),
         axis.text.y = element_text(size = 12, color = "black"))-> f3; f3
 
-### Climographs
-
-read.csv("data/temporal-survey-temperatures.csv") %>%
-  mutate(Time = as.POSIXct(Time, tz = "UTC")) %>%
-  as_tbl_time(index = Time) %>%
-  arrange(Time) %>%
-  filter_time("2009" ~ "2018") %>%
-  mutate(m = month(Time)) %>%
-  group_by(Site, m) %>%
-  summarise(T = mean(Temperature)) -> ts # Monthly average temperature
-
-read.csv("data/temporal-survey-wp.csv") %>%
-  mutate(m = month(Time)) %>%
-  group_by(Site, m) %>%
-  summarise(WP = mean(WP)) -> wp # Monthly average water potential
-
-merge(ts, wp) %>%
-  mutate(Site = fct_relevel(Site,
-                            "Los Cazadores", "Hou Sin Tierri",
-                            "Los Boches", "Hoyo Sin Tierra")) %>%
-  mutate(Site = fct_recode(Site, "Ḥou Sin Tierri" = "Hou Sin Tierri")) %>%
-  ggplot(aes(x = m)) +
-  facet_wrap(~ Site, nrow = 1) +
-  geom_bar(aes(y = -WP*10), stat = "identity", position = "dodge", fill = "skyblue", size = 1.1) +
-  geom_line(aes(y = T), color = "lightcoral", size = .5) +
-  geom_point(aes(y = T), color = "lightcoral", size = .7) +
-  scale_y_continuous(sec.axis = sec_axis( trans = ~./10, name = "Water potential (-MPa)")) +
-  ggthemes::theme_tufte() +
-  theme(strip.background = element_rect(colour = "grey96", fill = "grey96"),
-        legend.position = "none", 
-        legend.direction = "vertical",
-        legend.title = element_blank(),
-        legend.text = element_text(size = 16, face = "italic"), 
-        panel.background = element_rect(color = "grey96", fill = "grey96"),
-        strip.text = element_text(size = 12),
-        axis.title = element_text(size = 12),
-        axis.text.x = element_text(size = 8, color = "black"),
-        axis.text.y = element_text(size = 12, color = "black")) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  labs(x = "Calendar month (January to December)", y = "Temperature (ºC)") +
-  labs(title = "(A) Monthly climatic averages, temporal survey (2009-2018)") +
-  scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,10,11,12), ) -> f4; f4
-
 ### Combine panels
 
-cowplot::plot_grid(f4, f3, f1, f2, ncol = 1) -> fig
+cowplot::plot_grid(f1, f2, f3, ncol = 1) -> fig
 
 ### Save figure
 
 ggsave(fig, file = "results/figures/temperature-logs.png", 
-       path = NULL, scale = 1, width = 182, height = 240, units = "mm", dpi = 600)
+       path = NULL, scale = 1, width = 182, height = 180, units = "mm", dpi = 600)
 # ggsave(fig, file = "results/figures/temperature-logs.tiff", device = grDevices::tiff, 
 #        path = NULL, scale = 1, width = 182, height = 160, units = "mm", dpi = 600, compression = "lzw")
 
